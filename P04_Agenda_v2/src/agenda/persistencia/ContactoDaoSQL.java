8c6fd4ef8265ef2d3a14e592760c8c43e8e3a47f
sandra <sandradelakruz@gmail.com>
2023-02-08 16:36:50 +0100

    ejercicios Pablo

---
+++ P04_Agenda_v2/src/agenda/persistencia/ContactoDaoSQL.java
@@ -0,0 +1,26 @@
+package agenda.persistencia;
+
+import agenda.modelo.Contacto;
+
+public class ContactoDaoSQL implements ContactoDao {
+
+	@Override
+	public void insertar(Contacto c) {
+		System.out.println("insert into contactos ...");
+		System.out.println("se ha guardado en Mysql");
+		
+	}
+
+	@Override
+	public boolean eliminar(int idContacto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Contacto buscar(int idContacto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
