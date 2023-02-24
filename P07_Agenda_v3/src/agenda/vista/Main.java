2a23a7fc151d871960eaeab627a48f563659f6b5
sandra <sandradelakruz@gmail.com>
2023-02-24 17:26:55 +0100

    new vista added

--- P07_Agenda_v3/src/agenda/vista/Main.java
+++
@@ -1,8 +0,0 @@
-package agenda.vista;
-
-public class Main {
-	
-	public static void main(String[] args) {
-		new MenuPrincipal();
-	}
-}
--- P07_Agenda_v3/src/agenda/vista/MenuPrincipal.java
+++ P07_Agenda_v3/src/agenda/vista/MenuPrincipal.java
@@ -1,70 +1,44 @@
 package agenda.vista;
 
-import java.util.Scanner;
-
-import agenda.modelo.Contacto;
 import agenda.negocio.Agenda;
 import agenda.negocio.AgendaImpl;
+import util.Util;
 
 public class MenuPrincipal {
 	
-	private Agenda neg = new AgendaImpl();
-	
-	private Scanner tec = new Scanner(System.in);
+	private Agenda agenda;
 	
 	public MenuPrincipal() {
-		menu();
+		agenda = new AgendaImpl();
 	}
-
-	private void menu() {
-		System.out.println("Super Agenda 86");
-		System.out.println("---------------");
-		
+	
+	public void menu() {
+		System.out.println("SUPER AGENDA XX 7 PLUS");
+		System.out.println("----------------------");
 		boolean salir = false;
 		int opcion;
+		
 		do {
-			System.out.println("menu principal");
-			System.out.println("1 - nuevo contacto");
-			System.out.println("2 - buscar contacto");
-			System.out.println("9 - salir");
-			System.out.print("> ");
-			opcion = Integer.parseInt(tec.nextLine());
+			System.out.println("\nMenu Principal");
+			System.out.println("1 - Nuevo Contacto");
+			System.out.println("2 - Buscar contactos");
+			System.out.println("3 - Listar todos");
+			System.out.println("4 - Eliminar Contacto");
+			System.out.println("5 - Importar contactos");
+			System.out.println("9 - Salir");
+			System.out.println("Opción: ");
+			opcion = Util.leerInt();
 			
-			switch(opcion) {
+			switch (opcion) {
 			case 1:
-				nuevo();
+				new NuevoContacto(agenda);
 				break;
-			case 2:
-				buscar();
+			case 3:
+				new ConsultarTodos(agenda);
 				break;
 			case 9:
 				salir = true;
-				break;
-			default:
-				break;
 			}
-			
 		} while(!salir);
-	}
-	
-	private void nuevo() {
-		System.out.println("\nNuevo Contacto");
-		System.out.println("Ingresa los datos del nuevo contacto.");
-		System.out.print("Nombre: ");
-		String nom = tec.nextLine();
-		System.out.print("Apellidos: ");
-		String ape = tec.nextLine();
-		System.out.print("Apodo: ");
-		String apodo = tec.nextLine();
-		
-		Contacto nuevo = new Contacto();
-		nuevo.setNombre(nom);
-		nuevo.setApellidos(ape);
-		nuevo.setApodo(apodo);
-		
-		neg.insertarContacto(nuevo);
-	}
-	private void buscar() {
-		System.out.println("buscar");
 	}
 }
