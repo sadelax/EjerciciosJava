3cdf97c18722ed02ffd374c87528cab5fa75a613
sandra <sandradelakruz@gmail.com>
2023-02-24 17:28:41 +0100

    new package 'inicio'

---
+++ P07_Agenda_v3/src/agenda/inicio/InicioConsola.java
@@ -0,0 +1,10 @@
+package agenda.inicio;
+
+import agenda.vista.MenuPrincipal;
+
+public class InicioConsola {
+	public static void main(String[] args) {
+		MenuPrincipal mp = new MenuPrincipal();
+		mp.menu();
+	}
+}
---
+++ P07_Agenda_v3/src/agenda/inicio/InicioGrafico.java
@@ -0,0 +1,9 @@
+package agenda.inicio;
+
+import agenda.vista.swing.VInicial;
+
+public class InicioGrafico {
+	public static void main(String[] args) {
+		new VInicial();
+	}
+}
