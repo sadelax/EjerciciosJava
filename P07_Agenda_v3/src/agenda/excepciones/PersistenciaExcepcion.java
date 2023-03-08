4de35274045061179d5d757026571e31d2e37d81
sandra <sandradelakruz@gmail.com>
2023-03-08 21:03:08 +0100

    excepcion propia extends runtime

---
+++ P07_Agenda_v3/src/agenda/excepciones/PersistenciaExcepcion.java
@@ -0,0 +1,27 @@
+package agenda.excepciones;
+
+public class PersistenciaExcepcion extends RuntimeException {
+
+	public PersistenciaExcepcion() {
+		super();
+	}
+
+	public PersistenciaExcepcion(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public PersistenciaExcepcion(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PersistenciaExcepcion(String message) {
+		super(message);
+	}
+
+	public PersistenciaExcepcion(Throwable cause) {
+		super(cause);
+	}
+
+	
+}
