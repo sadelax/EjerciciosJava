1f8a66eab5abd3e982a0612711357be95593db90
sadelax <sade-la-@student.42madrid.com>
2023-03-25 15:03:41 +0100

    TO-DO

--- P07_Agenda_v3/src/agenda/persistencia/ContactoDaoJPA.java
+++ P07_Agenda_v3/src/agenda/persistencia/ContactoDaoJPA.java
@@ -3,39 +3,59 @@ package agenda.persistencia;
 import java.util.Set;
 import java.util.TreeSet;
 
+import javax.persistence.EntityManager;
+
+import agenda.config.Config;
 import agenda.modelo.Contacto;
 
 public class ContactoDaoJPA implements ContactoDao {
 	
-	
-	public ContactoDaoJPA() {
-		
-	}
+	private EntityManager em;
 
 	@Override
 	public void insertar(Contacto c) {
-
+		em = Config.getEMF().createEntityManager();
+		em.getTransaction().begin();
+		em.persist(c);
+		em.getTransaction().commit();
+		em.close();
 	}
 
 	@Override
 	public boolean eliminar(int idContacto) {
+		em = Config.getEMF().createEntityManager();
+		Contacto c = em.find(Contacto.class, idContacto);
+		if (c == null) {
+			em.close();
+			return false;
+		}
+		em.getTransaction().begin();
+		em.remove(c);
+		em.getTransaction().commit();
+		em.close();
 		return true;
 	}
 
 	@Override
 	public Contacto buscar(int idContacto) {
-		Contacto encontrado = new Contacto();
-		
-		return encontrado;
+		em = Config.getEMF().createEntityManager();
+		Contacto c = em.find(Contacto.class, idContacto);
+		em.close();
+		return c;
 	}
 
 	@Override
 	public void actualizar(Contacto contacto) {
-		
+		em = Config.getEMF().createEntityManager();
+		em.getTransaction().begin();
+		em.merge(contacto);
+		em.getTransaction().commit();
+		em.close();
 	}
 
 	@Override
 	public Set<Contacto> buscar(String nom) {
+		// TODO
 		Set<Contacto> contactos = new TreeSet<>();
 				
 		return contactos;
@@ -43,9 +63,12 @@ public class ContactoDaoJPA implements ContactoDao {
 	
 	@Override
 	public Set<Contacto> buscarTodos() {
-		Set<Contacto> contactos = new TreeSet<>();
-				
-		return contactos;
+		em = Config.getEMF().createEntityManager();
+		String sql = "SELECT c FROM Contacto c";
+		// TODO
+		Set<Contacto> result = new TreeSet<>();
+		
+		return result;
 	}
 	
 }
