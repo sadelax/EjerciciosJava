3cdc7370a57810720b6be83ff7778aebc277ff89
sadelax <sade-la-@student.42madrid.com>
2023-03-26 18:02:12 +0200

    junit test failed

--- P19_Banco_Spring/src/main/java/es/cursogetafe/banco/modelo/test/TestCarga.java
+++ P19_Banco_Spring/src/main/java/es/cursogetafe/banco/modelo/test/TestCarga.java
@@ -8,21 +8,15 @@ import es.cursogetafe.banco.modelo.Cliente;
 import es.cursogetafe.banco.modelo.Genero;
 
 public class TestCarga {
-
+	
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Banco_Spring");
 		EntityManager em = emf.createEntityManager();
 		
-		Cliente c = new Cliente();
-		c.setNombre("sandra");
-		c.setApellido1("delacruZ");
-		c.setNif("50770006g");
-		c.setSexo(Genero.F);
-		c.setMunicipio("Madrid");
-		c.setProvincia("madrid");
+		Cliente cliente = new Cliente("sandra", "delac", "507700161d", Genero.F, "madrid", "madrid");
 		
 		em.getTransaction().begin();
-		em.persist(c);
+		em.persist(cliente);
 		em.getTransaction().commit();
 		
 		em.close();
---
+++ P19_Banco_Spring/src/main/java/es/cursogetafe/banco/modelo/test/TestCargaJunit.java
@@ -0,0 +1,50 @@
+package es.cursogetafe.banco.modelo.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import es.cursogetafe.banco.modelo.Cliente;
+import es.cursogetafe.banco.modelo.Genero;
+
+public class TestCargaJunit {
+	
+	private EntityManagerFactory emf;
+	private EntityManager em;
+
+	@Before
+	public void setUp() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Banco_Spring");
+		EntityManager em = emf.createEntityManager();
+	}
+	
+	@After
+	public void tearDown() {
+		em.close();
+		emf.close();
+	}
+	
+	@Test
+	public void testCargaCliente() {
+		Cliente c = new Cliente("sandra", "delac", "50770001d", Genero.F, "madrid", "madrid");
+		
+		em.getTransaction().begin();
+		em.persist(c);
+		em.getTransaction().commit();
+		
+		Cliente clienteRecuperado = em.find(Cliente.class, c.getIdCliente());
+		
+		assertEquals(c.getNombre(), clienteRecuperado.getNombre());
+		assertEquals(c.getApellido1(), clienteRecuperado.getApellido1());
+		assertEquals(c.getNif(), clienteRecuperado.getNif());
+		assertEquals(c.getSexo(), clienteRecuperado.getSexo());
+		assertEquals(c.getProvincia(), clienteRecuperado.getProvincia());
+		assertEquals(c.getMunicipio(), clienteRecuperado.getMunicipio());
+	}
+}
