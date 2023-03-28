66925f2b9b05273621b3b79f8f04aa656eeb2532
sandra <you@example.com>
2023-03-28 16:22:57 +0200

    hoy

--- P19_Banco_Spring/src/main/java/es/cursogetafe/banco/persistencia/test/TestMapeo.java
+++ P19_Banco_Spring/src/main/java/es/cursogetafe/banco/persistencia/test/TestMapeo.java
@@ -5,7 +5,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import es.cursogetafe.banco.modelo.Cliente;
 import es.cursogetafe.banco.modelo.Cuenta;
