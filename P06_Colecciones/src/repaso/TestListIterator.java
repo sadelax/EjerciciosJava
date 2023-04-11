53a5a413664c2c21f282c88cf3a1e2c215b21690
sandra <you@example.com>
2023-04-11 16:37:42 +0200

    iterator repaso

---
+++ P06_Colecciones/src/repaso/TestListIterator.java
@@ -0,0 +1,62 @@
+package repaso;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TestListIterator {
+
+	public static void main(String[] args) {
+		
+		List<Persona> personas;
+		
+//		personas = new ArrayList<>();
+		personas = new LinkedList<>();
+		
+		personas.add(new Persona(75, "AA"));
+		personas.add(new Persona(76, "Aj"));
+		personas.add(new Persona(756, "Ah"));
+		personas.add(new Persona(751, "Ag"));
+		personas.add(new Persona(759, "Af"));
+		personas.add(new Persona(754, "Ab"));
+		
+		// PARA QUÉ ES BUENO EL ARRAYLIST? Para hacer búsquedas por posición.
+		// PARA QUÉ ES BUENO EL LINKEDLIST? Para inserción y eliminación eficiente en el medio de la lista.
+		// Y CÓMO LOS ORDENAMOS? Con Collection.sort.
+			// OJO! IMPRESCINDIBLE que la clase implemente COMPARABLE
+			// necesito elementos repetidos? usa list, por favor.
+			// necesito que los elementos no se repitan? usa treeset.
+		
+		for (Persona persona : personas) {
+			System.out.println(persona);
+		}
+		
+		Collections.sort(personas);
+		Collections.sort(personas, new Comparator<Persona>() {
+
+			@Override
+			public int compare(Persona p1, Persona p2) {
+				return p1.getNombre().compareTo(p2.getNombre()); // ojo, por orden ascii, sin español ni na
+			}
+			
+		});
+		
+		for (Persona persona : personas) {
+			System.out.println(persona);
+		}
+		
+		// EXPRESIÓN LAMBDA DE LO ANTERIOR
+		
+		Collections.sort(personas, (p1,p2) -> p2.getIdPersona() - p2.getIdPersona());
+		
+		personas.forEach(System.out::println);
+	}
+	
+}
