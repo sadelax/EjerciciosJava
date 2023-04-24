4e6da29f7202b6f4dc05a707f314927946f5f7ed
sandra <sade-la-@student.42madrid.com>
2023-04-24 16:36:43 +0200

    controlador con init

--- W06_Registro/src/main/java/vista/Controller.java
+++ W06_Registro/src/main/java/vista/Controller.java
@@ -12,21 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/home/*")
 @SuppressWarnings("serial")
 public class Controller extends HttpServlet {
+
+	public Controller(){
+		System.out.println("se ejecutó el constructor del servlet");
+	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		if(getServletContext().getAttribute("config") == null) {
-			setApp();
-		}
+		
 	}
 	
-	private void setApp() {
+	@Override
+	public void init() throws ServletException {
 		ServletContext app = getServletContext();
 		app.setAttribute("context", app.getContextPath());
 		app.setAttribute("home", app.getContextPath() + "/home");
 		app.setAttribute("js", app.getContextPath() + "/js");
 		app.setAttribute("css", app.getContextPath() + "/css");
 		app.setAttribute("images", app.getContextPath() + "/images");
-		app.setAttribute("config", "ok");
 	}
 }
